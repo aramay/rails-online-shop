fbad26d65ac7204b4f2ce7c3e63f774c8fecd192
Jonathan Hall & Ted Day-Fratto <pair+jlhall.tedsf@devbootcamp.com>
2016-07-13 11:33:13 -0700

    Update applications.js to fix heroku deplayment issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-= require jquery
-= require bootstrap
 //= require jquery_ujs
 //= require_tree .
