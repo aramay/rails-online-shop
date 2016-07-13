c7a65e2c52dc2986499fb92fc1b24c6cba9db0aa
Jonathan Hall & Ted Day-Fratto <pair+jlhall.tedsf@devbootcamp.com>
2016-07-12 17:15:36 -0700

    Add quantity to db and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,7 +41,7 @@ class ProductsController < ApplicationController
 
 private
   def product_params
-    params.require(:product).permit(:name, :price, :description, :image)
+    params.require(:product).permit(:name, :price, :description, :quantity, :image)
   end
 
   def find_product
