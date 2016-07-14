964527f19c2352ab888a3d39f7925a74926694d4
Jonathan Hall & Ted Day-Fratto <pair+jlhall.tedsf@devbootcamp.com>
2016-07-14 10:39:17 -0700

    Add user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
+
   def create
-    @user = User.create(params[:user])
+    @user = User.new(user_params)
+    p @user
     if @user.save
-      redirect root_path
+      p @user
+      redirect_to root_path
     else
-      redirect new_user_path
+      p @user
+      redirect_to new_user_path
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   validates :username, presence: true
   validates :email, presence: true
-  validates :admin, presence: true
   has_secure_password
 end
