810f42e15ca1c1b99feb681a15afed577e0a0b0b
Abid Ramay <kasper_341@hotmail.com>
2016-07-14 19:50:14 -0700

    Update routes to use devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :products, :users, :sessions, :categories
+  devise_for :users
+
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  # devise_for :users
+  # resources :products, :users, :sessions, :categories
+  resources :products, :categories
 
   root "products#index"
 end
