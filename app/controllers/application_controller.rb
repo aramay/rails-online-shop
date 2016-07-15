e16bbfed4d92df4a3522c9ceffecd8f50e987ebb
paulunits <obsolete.units@gmail.com>
2016-07-15 15:56:15 -0700

    Fix app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # protect_from_forgery with: :null_session
+  helper_method :current_order
   
   def current_order
     if !session[:order_id].nil?
