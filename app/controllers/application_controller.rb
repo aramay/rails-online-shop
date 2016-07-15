ad3378d3662d8576723c10b1b556b82328511a2e
paulunits <obsolete.units@gmail.com>
2016-07-15 15:23:15 -0700

    Adding shopping cart functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # protect_from_forgery with: :null_session
+  
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
 
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users
   # devise_for :users
   # resources :products, :users, :sessions, :categories
   resources :products, :categories
-
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
   root "products#index"
 end
