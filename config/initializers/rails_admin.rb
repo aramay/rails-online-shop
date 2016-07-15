f2ab41ba8704b110f3fcd2436b85384be476535b
Abid Ramay & Chris Wong <pair+aramay.chris-wong-1@devbootcamp.com>
2016-07-14 18:39:24 -0700

    push to heroku

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,23 +1,22 @@
 RailsAdmin.config do |config|
-  config.authenticate_with do
-    authenticate_or_request_with_http_basic('Site Message') do |username, password|
-      username == 'admin' && password == 'secret'
+  # config.authenticate_with do
+  #   authenticate_or_request_with_http_basic('Site Message') do |username, password|
+  #     username == 'admin' && password == 'secret'
+  #   end
+
+  config.current_user do
+    user = User.find(session[:user_id])
+    if user
+      user
+    else
+      flash[:danger] = "You need to login to access"
+      redirect_to root_path
     end
   end
 
-  # config.current_user do
-  #   user = User.find(session[:user_id])
-  #   if user
-  #     user
-  #   else
-  #     flash[:danger] = "You need to login to access"
-  #     redirect_to root_path
-  #   end
-  # end
-
-  # config.authorize_with do
-  #   redirect_to root_path unless current_user.try(:admin?)
-  # end
+  config.authorize_with do
+    redirect_to root_path unless current_user.try(:admin?)
+  end
 
   config.actions do
     dashboard                     # mandatory
@@ -30,4 +29,5 @@ RailsAdmin.config do |config|
     delete
     show_in_app
   end
+
 end
