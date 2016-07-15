ebad544e632f4b4baf9ff7f902ee1bfdbdd2dc73
“kasper341” <kasper_341@hotmail.com>
2016-07-15 00:39:20 -0700

    Change "sign_out" route to use 'get'

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,21 @@
 RailsAdmin.config do |config|
 
+  RailsAdmin.config do |config|
+    config.authorize_with do
+
+      if current_user
+        redirect_to main_app.root_path unless current_user.admin == true
+      else
+        redirect_to main_app.root_path
+      end
+    end
+
+    # config.authorize_with do
+    #   redirect_to main_app.root_path unless current_user.admin == true
+    # end
+
+end
+
   ### Popular gems integration
 
   ## == Devise ==
