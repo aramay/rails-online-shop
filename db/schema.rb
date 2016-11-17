849c71a9c12dcb391bdabb9060786f80fa64d1fe
“kasper341” <kasper_341@hotmail.com>
2016-11-17 15:34:55 -0800

    add migration to combine items in carts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117213126) do
+ActiveRecord::Schema.define(version: 20161117215552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
