38641a6bc23f7d5ed3d003f1403c0510bc2986e4
“kasper341” <kasper_341@hotmail.com>
2016-11-17 12:17:43 -0800

    add association and hook method 'ensure_not_referenced_by_any_line_item'

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,8 +8,24 @@ class Product < ActiveRecord::Base
   has_many :category_products
   has_many :categories, through: :category_products
   has_many :order_items
-  
+
   belongs_to :category
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+          return true
+      else
+          errors.add(:base, 'Line Items present')
+          return false
+      end
+
+  end
+
 
   # default_scope { where(active: true) }
 
