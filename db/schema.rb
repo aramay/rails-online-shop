31089d79dfda63f12e212e815431ba4e956dd580
“kasper341” <kasper_341@hotmail.com>
2016-11-17 12:18:36 -0800

    update schema for 'cart' and 'line_item' models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117193307) do
+ActiveRecord::Schema.define(version: 20161117193946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20161117193307) do
 
   add_index "category_products", ["category_id"], name: "index_category_products_on_category_id", using: :btree
   add_index "category_products", ["product_id"], name: "index_category_products_on_product_id", using: :btree
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "product_id"
