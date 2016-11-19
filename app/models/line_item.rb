71ec1e5dd21b138d4d8f0ac7b1254242076b6d9a
“kasper341” <kasper_341@hotmail.com>
2016-11-19 08:12:29 -0800

    add association with orders

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,8 +2,10 @@ class LineItem < ActiveRecord::Base
     belongs_to :cart
     belongs_to :product
 
+    belongs_to :order
+
     def total_price
         product.price * quantity
     end
-    
+
 end
