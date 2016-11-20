1fee3660d7e240dfbd803a5b568aba7947bcc10e
“kasper341” <kasper_341@hotmail.com>
2016-11-20 00:09:17 -0800

    add authentication to admin console

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,15 +1,11 @@
 RailsAdmin.config do |config|
-  config.authorize_with do
+    config.authorize_with do
     if current_user
       redirect_to main_app.root_path unless current_user.admin == true
     else
       redirect_to main_app.root_path
     end
   end
-
-  # config.authorize_with do
-  #   redirect_to main_app.root_path unless current_user.admin == true
-  # end
 
   ### Popular gems integration
 
@@ -29,6 +25,10 @@ RailsAdmin.config do |config|
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
+  ## == Gravatar integration ==
+  ## To disable Gravatar integration in Navigation Bar set to false
+  # config.show_gravatar true
 
   config.actions do
     dashboard                     # mandatory
