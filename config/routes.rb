433132350af113fed791fd1aa607a629e08c6a7a
“kasper341” <kasper_341@hotmail.com>
2016-11-20 00:09:42 -0800

    mount 'rails-admin'

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+
   resources :orders
   resources :line_items
   resources :carts
-  get 'order_items/create'
-
-  get 'order_items/update'
-
-  get 'order_items/destroy'
+  # get 'order_items/create'
+  #
+  # get 'order_items/update'
+  #
+  # get 'order_items/destroy'
 
   get 'carts/show'
-
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users
   # devise_for :users
   # resources :products, :users, :sessions, :categories
   resources :products, :categories
   resource :cart, only: [:show, :update]
-  resources :order_items, only: [:create, :update, :destroy]
+  # resources :order_items, only: [:create, :update, :destroy]
   root "products#index"
 end
