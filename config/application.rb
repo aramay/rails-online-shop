85e782da27bb21c59afd20675c08676092a03e43
“kasper341” <kasper_341@hotmail.com>
2017-03-17 08:49:34 -0700

    update tests config

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,17 @@ module Shoppe
   class Application < Rails::Application
     config.generators.helper = false
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
+
 end
